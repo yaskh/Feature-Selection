42911a11190998098d7062555bcdbf462b0e8f99
yaskh <khatri.yasir@gmail.com>
2019-05-25 02:53:09 +0500

    Heatmap for features

--- features selection.py
+++ features selection.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("train.csv",sep = ',')
 
@@ -17,3 +19,16 @@ dfcolumns = pd.DataFrame(X.columns)
 
 featureScores = pd.concat([dfcolumns,dfscores],axis=1)
 featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+featureScores.nsmallest(10,'Score')
+print(featureScores.nl(10,'Score'))
+
+import matplotlib.pyplot as plt
+heatmap = data.corr()
+heatmap.index
+
+
+corrmat = data.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(20,20))
+#plot heat map
+g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
